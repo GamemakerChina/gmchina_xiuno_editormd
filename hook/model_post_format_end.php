590e07b1b9e48341da06a47b5a8ae46361bfcf16
Mark Shi <qq1062787843@hotmail.com>
2018-12-23 22:49:53 +0800

    Update model_post_format_end.php

--- hook/model_post_format_end.php
+++ hook/model_post_format_end.php
@@ -2,6 +2,7 @@ include_once 'plugin/gmchina_xiuno_editormd/parser/1.7.1/Parsedown.php';
 include_once 'plugin/gmchina_xiuno_editormd/parser/1.7.1/ParsedownExtra.php';
 $Parsedown = new Parsedown();
 $Extra = new ParsedownExtra();
+$Parsedown->setSafeMode(true); //Safe Mode(若使用 1.5.4 版本需移除或注释)
 
 if($post['doctype'] == 2){
     $post['message_fmt'] = $Extra->text($post['message_fmt']);
