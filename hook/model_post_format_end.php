a56a77612e39a1d80b2f7704d386c2110e730267
liaronce <qq1062787843@hotmail.com>
2018-12-24 13:29:01 +0800

    加入 HTML Purifier 过滤输出 XSS

--- hook/model_post_format_end.php
+++ hook/model_post_format_end.php
@@ -1,8 +1,12 @@
 include_once 'plugin/gmchina_xiuno_editormd/parser/1.7.1/Parsedown.php';
 include_once 'plugin/gmchina_xiuno_editormd/parser/1.7.1/ParsedownExtra.php';
+include_once 'plugin/gmchina_xiuno_editormd/purifier/HTMLPurifier.standalone.php';
 $Parsedown = new Parsedown();
+$Parsedown->setSafeMode(true);
 $Extra = new ParsedownExtra();
+$config = HTMLPurifier_Config::createDefault();
+$purifier = new HTMLPurifier($config);
 
 if($post['doctype'] == 2){
-    $post['message_fmt'] = $Extra->text($post['message_fmt']);
+    $post['message_fmt'] = $purifier->purify($Extra->text($post['message_fmt']));
 }
