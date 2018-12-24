2173c65fd18b03dd15101b280a635fb9a83fa524
liaronce <qq1062787843@hotmail.com>
2018-12-24 13:32:59 +0800

    重新加入注释

--- hook/model_post_format_end.php
+++ hook/model_post_format_end.php
@@ -2,7 +2,7 @@ include_once 'plugin/gmchina_xiuno_editormd/parser/1.7.1/Parsedown.php';
 include_once 'plugin/gmchina_xiuno_editormd/parser/1.7.1/ParsedownExtra.php';
 include_once 'plugin/gmchina_xiuno_editormd/purifier/HTMLPurifier.standalone.php';
 $Parsedown = new Parsedown();
-$Parsedown->setSafeMode(true);
+$Parsedown->setSafeMode(true);  //Safe Mode(若使用 1.5.4 版本需移除或注释)
 $Extra = new ParsedownExtra();
 $config = HTMLPurifier_Config::createDefault();
 $purifier = new HTMLPurifier($config);
