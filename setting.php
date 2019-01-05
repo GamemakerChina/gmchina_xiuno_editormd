7923fbd36674bd4502395b75a5d11f5c40c38cf7
liaronce <qq1062787843@hotmail.com>
2019-01-05 13:31:49 +0800

    设置界面完成

--- setting.php
+++ setting.php
@@ -8,6 +8,7 @@ $width = param('width');
 $height = param('height');
 $img_upload_name = param('img_upload_name');
 $html_decode = param('html_decoden');
+$html_decode_fliter = param('html_decode_fliter');
 $sync_scrolling = param('sync_scrolling');
 $custom_toolbar = param('custom_toolbar');
 $custom_toolbar_onoff = param('custom_toolbar_onoff');
@@ -37,6 +38,7 @@ if($method == 'GET') {
             'height'=>$height,
             'img_upload_name'=>$img_upload_name,
             'html_decode'=>$html_decode,
+            'html_decode_fliter'=>$html_decode_fliter,
             'sync_scrolling'=>$sync_scrolling,
             'custom_toolbar'=>$custom_toolbar,
             'custom_toolbar_onoff'=>$custom_toolbar_onoff,
@@ -61,43 +63,44 @@ if($method == 'GET') {
     //Parsedown 版本切换
     $parsedown_version = form_radio('sync_scrolling', array('1.8.0-beta5'=>'1.8.0-beta5', '1.7.1'=>'1.7.1', '1.5.4'=>'1.5.4（用于 PHP5.2）'), '1.7.1');
     //图片上传
-    $img_upload = form_radio_yes_no('img_upload', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $img_upload = form_radio_yes_no('img_upload', $editor_md_config['img_upload'], 0);
     $img_upload_name = form_text('img_upload_name', $editor_md_config['img_upload_name'], '100%', '使用 ‘,’ 隔开');
     $image_upload_url = form_text('image_upload_url', $editor_md_config['image_upload_url'], '100%', '');
-    $cross_domain_upload = form_radio_yes_no('cross_domain_upload', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $cross_domain_upload = form_radio_yes_no('cross_domain_upload', $editor_md_config['cross_domain_upload'], 0);
     $upload_callback_url = form_text('upload_callback_url', $editor_md_config['upload_callback_url'], '100%', '');
     //滚动
-    $sync_scrolling = form_radio('sync_scrolling', array('true'=>'双向滚动', '"single"'=>'单向滚动', 'false'=>'关闭'), 'false');
+    $sync_scrolling = form_radio('sync_scrolling', array(1=>'双向滚动', 2=>'单向滚动', 0=>'关闭'), $editor_md_config['sync_scrolling']);
     //长度宽度
     $width = form_text('width', $editor_md_config['width'], '100%', '若为百分比需带双引号');
     $height = form_text('height', $editor_md_config['height'], '100%', '若为百分比需带双引号');
     //HTML解析
-    $html_decode = form_text('html_decode', $editor_md_config['html_decode'], '100%', '使用 ‘,’ 隔开');
+    $html_decode = form_radio_yes_no('html_decode', $editor_md_config['html_decode'], 0);
+    $html_decode_fliter = form_text('html_decode_fliter', $editor_md_config['html_decode_fliter'], '100%', '使用 ‘,’ 隔开');
     //自定义工具栏
-    $custom_toolbar_onoff = form_radio_yes_no('custom_toolbar_onoff', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $custom_toolbar_onoff = form_radio_yes_no('custom_toolbar_onoff', $editor_md_config['custom_toolbar_onoff'], 0);
     $custom_toolbar = form_textarea('custom_toolbar', $editor_md_config['custom_toolbar']);
     //自动高度
-    $autoheight = form_radio_yes_no('autoheight', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $autoheight = form_radio_yes_no('autoheight', $editor_md_config['autoheight'], 0);
     //只读模式（可能维护的时候有用）
-    $readonly = form_radio_yes_no('readonly', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $readonly = form_radio_yes_no('readonly', $editor_md_config['readonly'], 0);
     //编辑器主题
-    $theme_onoff = form_radio_yes_no('theme_onoff', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $theme_onoff = form_radio_yes_no('theme_onoff', $editor_md_config['theme_onoff'], 0);
     $theme = form_text('theme', $editor_md_config['theme'], '100%', '');
     //代码折叠
-    $codefold = form_radio_yes_no('codefold', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $codefold = form_radio_yes_no('codefold', $editor_md_config['codefold'], 0);
     //任务列表（GFM 功能）
-    $tasklist = form_radio_yes_no('tasklist', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $tasklist = form_radio_yes_no('tasklist', $editor_md_config['tasklist'], 0);
     //@链接
-    $atLink = form_radio_yes_no('atLink', array('true'=>'开启', 'false'=>'关闭'), 'false');
-    $emailLink = form_radio_yes_no('emailLink', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $atLink = form_radio_yes_no('atLink', $editor_md_config['atLink'], 0);
+    $emailLink = form_radio_yes_no('emailLink', $editor_md_config['emailLink'], 0);
     //TeX / LaTeX 科学公式语言
-    $tex = form_radio_yes_no('tex', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $tex = form_radio_yes_no('tex', $editor_md_config['tex'], 0);
     //FlowChart 流程图
-    $flowChart = form_radio_yes_no('flowChart', array('true'=>'开启', 'false'=>'关闭'), 'true');
+    $flowChart = form_radio_yes_no('flowChart', $editor_md_config['flowChart'], 1);
     //SequenceDiagram 时序图 / 序列图
-    $sequenceDiagram = form_radio_yes_no('sequenceDiagram', array('true'=>'开启', 'false'=>'关闭'), 'true');
+    $sequenceDiagram = form_radio_yes_no('sequenceDiagram', $editor_md_config['sequenceDiagram'], 1);
     //分页符
-    $pageBreak = form_radio_yes_no('pageBreak', array('true'=>'开启', 'false'=>'关闭'), 'false');
+    $pageBreak = form_radio_yes_no('pageBreak', $editor_md_config['pageBreak'], 0);
 
     include _include(APP_PATH.'plugin/gmchina_xiuno_editormd/setting.htm');
 } else {
@@ -111,6 +114,7 @@ if($method == 'GET') {
 	$editor_md_config['height'] = param('height');
 	$editor_md_config['sync_scrolling'] = param('sync_scrolling');
     $editor_md_config['html_decode'] = param('html_decode');
+    $editor_md_config['html_decode_fliter'] = param('html_decode_fliter');
     $editor_md_config['custom_toolbar_onoff'] = param('custom_toolbar_onoff');
     $editor_md_config['custom_toolbar'] = param('custom_toolbar');
     $editor_md_config['autoheight'] = param('autoheight');
